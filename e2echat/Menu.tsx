59d8f916fee1e24f7c2114989780103e298d3d15
Xavi-Hub <xavihub@gmail.com>
2020-02-22 19:53:13 -0800

    Merge remote-tracking branch 'remotes/origin/master' into menu
    # Conflicts:
    #	e2echat/App.tsx
    
    Starts on login page and switches to messages page
    Menu page has dummy list items

--- e2echat/Menu.tsx
+++ e2echat/Menu.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import {FlatList, Text, View} from 'react-native'
+import {Dimensions} from "react-native";
+
+let {width, height} = Dimensions.get("window");
 
 const MenuItem = ({item}) => {
 
+    return (
+        <View style={{flex: 1, width: width, height: 100}}>
+            <View style={{flex: 1, backgroundColor: 'rgba(50,50,50,1.0)'}}/>
+            <View style={{justifyContent: 'center', flex: 98}}>
+                <Text style={{fontSize: 32}}>{item.name}</Text>
+            </View>
+            <View style={{flex: 1, backgroundColor: 'rgba(50,50,50,1.0)'}}/>
+        </View>
+    );
 };
 
-const Menu = (item) => {
+const Menu = ({item}) => {
     const data = [
         {
             name: 'Xavi',
@@ -23,7 +35,7 @@ const Menu = (item) => {
 
     return (
         <View style={{justifyContent: 'center', alignContent: 'center', flex: 1}}>
-            {/*<FlatList data={data} renderItem={({item}) => <MenuItem item={item}/>}/>*/}
+            <FlatList data={data} renderItem={({item}) => <MenuItem item={item}/>}/>
         </View>
     );
 };
